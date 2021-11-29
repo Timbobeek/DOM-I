9248ff4e44e0677963e0d469af7d6bcfc488b9bc
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-11-28 18:57:00 -0600

    bottom conent to do

--- src/index.js
+++ src/index.js
@@ -41,6 +41,53 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 
 console.log('project wired!')
 
-const someText = document.querySelector('h1');
-someText.textContent = 'DOM Is Awesome';
+
+
+const nav = document.querySelectorAll("a");
+
+nav[0].textContent = 'Services';
+nav[1].textContent = 'Product';
+nav[2].textContent = 'Vision';
+nav[3].textContent = 'Features';
+nav[4].textContent = 'About';
+nav[5].textContent = 'Contact';
+
+nav.forEach(n => n.className = 'italic');
+
+const logo = document.querySelector('#logo-img');
+logo.src = "http://localhost:9000/img/logo.png";
+
+const mainHeader = document.querySelector('h1');
+mainHeader.textContent = 'DOM Is Awesome';
+
+const button = document.querySelector('button');
+button.textContent = 'Get Started';
+
+const image1 = document.querySelector('#cta-img');
+image1.src = "http://localhost:9000/img/cta.png";
+
+const contentHeaders = document.querySelectorAll('.main-content h4');
+contentHeaders[0].textContent = 'Features';
+contentHeaders[1].textContent = 'About';
+contentHeaders[2].textContent = 'Services';
+contentHeaders[3].textContent = 'Product';
+contentHeaders[4].textContent = 'Vision';
+
+const contentHeadersParagraphs = document.querySelectorAll('.main-content p');
+contentHeadersParagraphs[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+contentHeadersParagraphs[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+contentHeadersParagraphs[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+contentHeadersParagraphs[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+contentHeadersParagraphs[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const image2 = document.querySelector('#middle-img');
+image2.src = "http://localhost:9000/img/accent.png";
+
+
+
+
+
+
+
+
 
